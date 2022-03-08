d6e3cbd2ea5f147cb5da7cacc8ee4c52ca355ddb
Eleonora <eleonora.maccacaro@scilifelab.se>
2022-03-08 08:08:00 +0100

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+const axios = require('axios').default;
 
 function App() {
+
+useEffect(()=>{
+  axios.post('https://www.avanza.se/_api/fund-guide/list?shouldCheckFondExcludedFromPromotion=true', {"startIndex":0,"indexFund":true,"showActivelyManagedFunds":false,"sustainabilityProfile":false,"lowCo2":false,"svanenMark":false,"noFossilFuelInvolvement":false,"commonRegionFilter":[],"otherRegionFilter":[],"alignmentFilter":[],"industryFilter":[],"fundTypeFilter":["Aktiefond"],"interestTypeFilter":[],"sortField":"totalFee","sortDirection":"ASCENDING","name":"","recommendedHoldingPeriodFilter":[],"companyFilter":[],"productInvolvementsFilter":[],"ratingFilter":[],"sustainabilityRatingFilter":[],"environmentalRatingFilter":[],"socialRatingFilter":[],"governanceRatingFilter":[]})
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });})
   return (
     <div className="App">
       <header className="App-header">
