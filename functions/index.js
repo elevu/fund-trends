bc68cc65f721e6673a8cc0cb665426d0a2ff4628
Eleonora <eleonora.maccacaro@scilifelab.se>
2022-03-09 16:12:40 +0100

    Add first function

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require("firebase-functions");
+const fetch = require("node-fetch");
+
+// eslint-disable-next-line no-unused-vars
+exports.getFunds = functions.https.onRequest((request, response) => {
+  response.set("Access-Control-Allow-Origin", "*");
+  fetch("https://www.avanza.se/_api/fund-guide/list?shouldCheckFondExcludedFromPromotion=true", {
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    // eslint-disable-next-line max-len
+    body: JSON.stringify({"startIndex": 0, "indexFund": true, "showActivelyManagedFunds": false, "sustainabilityProfile": false, "lowCo2": false, "svanenMark": false, "noFossilFuelInvolvement": false, "commonRegionFilter": [], "otherRegionFilter": [], "alignmentFilter": [], "industryFilter": [], "fundTypeFilter": ["Aktiefond"], "interestTypeFilter": [], "sortField": "totalFee", "sortDirection": "ASCENDING", "name": "", "recommendedHoldingPeriodFilter": [], "companyFilter": [], "productInvolvementsFilter": [], "ratingFilter": [], "sustainabilityRatingFilter": [], "environmentalRatingFilter": [], "socialRatingFilter": [], "governanceRatingFilter": []}),
+  })
+      .then((res) => res.text())
+      .then((text) => response.send(text)
+      );
+});
--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 const axios = require('axios').default;
 
 function App() {
 
+  const [funds, setFunds] = useState()
+
 useEffect(()=>{
-  axios.post('https://www.avanza.se/_api/fund-guide/list?shouldCheckFondExcludedFromPromotion=true', {"startIndex":0,"indexFund":true,"showActivelyManagedFunds":false,"sustainabilityProfile":false,"lowCo2":false,"svanenMark":false,"noFossilFuelInvolvement":false,"commonRegionFilter":[],"otherRegionFilter":[],"alignmentFilter":[],"industryFilter":[],"fundTypeFilter":["Aktiefond"],"interestTypeFilter":[],"sortField":"totalFee","sortDirection":"ASCENDING","name":"","recommendedHoldingPeriodFilter":[],"companyFilter":[],"productInvolvementsFilter":[],"ratingFilter":[],"sustainabilityRatingFilter":[],"environmentalRatingFilter":[],"socialRatingFilter":[],"governanceRatingFilter":[]})
+  axios.get('http://localhost:5001/fund-trends/us-central1/getFunds')
   .then(function (response) {
-    console.log(response);
+    setFunds(response.data.fundListViews)
+    console.log(response)
   })
   .catch(function (error) {
     console.log(error);
-  });})
+  });},[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {funds && funds.map(fund=>fund.name)}
     </div>
   );
 }
