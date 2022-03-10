6e397146420b693925b86564adb496e7418ed381
Eleonora <eleonora.maccacaro@scilifelab.se>
2022-03-10 11:52:02 +0100

    Style homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import {Table} from "antd";
+import { StarOutlined } from '@ant-design/icons';
+
 const axios = require('axios').default;
 
 function App() {
@@ -23,6 +25,37 @@ const columns = [
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
+    render: (name, item) => <a href={`https://www.avanza.se/fonder/om-fonden.html/${item.orderbookId}/avanza-zero`}>{name}</a>
+  },
+  {
+    title: 'Momentum rank',
+    key: 'rank',
+    sorter: (a, b) => a.developmentOneWeek + a.developmentOneMonth + a.developmentThreeMonths - (b.developmentOneWeek + b.developmentOneMonth + b.developmentThreeMonths),
+    render: (item) => <div style={{color: item.developmentOneWeek + item.developmentOneMonth + item.developmentThreeMonths > 0 ? '#007f8f' : '#d0184d'}}>{parseFloat(item.developmentOneWeek?.toFixed(1)) + parseFloat(item.developmentOneMonth?.toFixed(1)) + parseFloat(item.developmentThreeMonths?.toFixed(1))}</div>
+  },
+  {
+    title: '1 week',
+    dataIndex: 'developmentOneWeek',
+    key: 'developmentOneWeek',
+    render: (amount) => <div style={{color: amount > 0 ? '#007f8f' : '#d0184d'}}>{amount.toFixed(1)}</div>
+  },
+  {
+    title: '1 month',
+    dataIndex: 'developmentOneMonth',
+    key: 'developmentOneMonth',
+    render: (amount) => <div style={{color: amount > 0 ? '#007f8f' : '#d0184d'}}>{amount.toFixed(1)}</div>
+  },
+  {
+    title: '3 months',
+    dataIndex: 'developmentThreeMonths',
+    key: 'developmentThreeMonths',
+    render: (amount) => <div style={{color: amount > 0 ? '#007f8f' : '#d0184d'}}>{amount.toFixed(1)}</div>
+  },
+  {
+    title: 'Total fee',
+    dataIndex: 'totalFee',
+    key: 'totalFee',
+    render: (fee) => <div>{fee}%</div>
   },
   {
     title: 'Category',
@@ -31,18 +64,8 @@ const columns = [
   },
   {
     title: 'Company',
-    dataIndex: 'company',
-    key: 'company',
-  },
-  {
-    title: 'Category',
-    dataIndex: 'category',
-    key: 'category',
-  },
-  {
-    title: 'Company',
-    dataIndex: 'company',
-    key: 'company',
+    dataIndex: 'companyName',
+    key: 'companyName',
   },
   {
     title: 'Currency',
@@ -50,14 +73,10 @@ const columns = [
     key: 'currencyCode',
   },
   {
-    title: 'Type',
-    dataIndex: 'fundType',
-    key: 'fundType',
-  },
-  {
     title: 'Rating',
     dataIndex: 'rating',
     key: 'rating',
+    render: (rate) => <>{[...Array(rate).keys()].map(()=><StarOutlined />)}</>
   },
   {
     title: 'risk',
@@ -67,8 +86,8 @@ const columns = [
 ];
 
   return (
-    <div className="App">
-      <Table dataSource={funds} columns={columns} />;
+    <div className="App" style={{padding: 50}}>
+      <Table dataSource={funds} columns={columns} rowKey={'name'} />;
     </div>
   );
 }
